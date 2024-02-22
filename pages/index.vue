9b3aa481b7bfd7ebfd13f63562ca4c68002fcb46
“Marco <marco.pianaroli96@gmail.com>
2024-02-22 10:11:55 +0100

    feature:add homepage

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+
+const count = ref(0)
+function increment() {
+  count.value++
+}
+</script>
+
+<template>
+  <h1>HOME PAGE</h1>
+  <h2>recently viewed</h2>
+  <h2>workspaces</h2>
+</template>
