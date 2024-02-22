820222a9f8549a02c7f439dd30ea145384b91dd4
“Marco <marco.pianaroli96@gmail.com>
2024-02-22 10:52:26 +0100

    feat:creates default layout

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+import { ref } from 'vue';
+
+const hello = ref('hello world')
+</script>
+
+<template>
+  <p>detfault layotu</p>
+  <nav>
+    <nuxt-link to="/">Home</nuxt-link>
+  </nav>
+  <slot />
+</template>
