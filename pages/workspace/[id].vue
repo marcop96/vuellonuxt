cca1de9c020b99e7925a2caec5cc3bfd2243ee22
“Marco <marco.pianaroli96@gmail.com>
2024-02-27 18:41:35 +0100

    feature: user can add columns and cards to respective columns

--- pages/workspace/[id].vue
+++ pages/workspace/[id].vue
@@ -6,27 +6,46 @@ import type { Workspace } from '../../types'
 
 const route = useRoute()
 const newBoardName = ref('')
+const newCardName = ref('')
 const board = ref({
   name: 'test',
-  columns: []
+  columns: [] as any
 });
 const workspace = (workspaceList.value.find((w: Workspace) => w.id === Number(route.params.id)) as Workspace)
 
 function createColumn() {
   board.value.columns.push({
-    name: newBoardName.value
+    name: newBoardName.value,
+    cards: []
   })
   newBoardName.value = ''
 }
+
+function createCard(column: any) {
+  (column.cards.push({ name: newCardName.value }))
+  console.log((column.cards))
+}
+
+
 </script>
 
 <template>
-  <h1>{{ workspace.name }} {{ workspace.id }}</h1>
+  <h1 class="text-center text-5xl">{{ workspace.name }} - {{ workspace.id }}</h1>
   <section>
-    <div class="column-grid grid grid-cols-4">
-
-    </div>
-    <!-- <input type="text" v-model="newBoardName" />
-    <button @click="createColumn" class="bg-red-500 rounded-lg">Create Column</button> -->
+    <input type="text" class="border border-2  p-2 mx-2" v-model="newBoardName" />
+    <button @click="createColumn" class="bg-red-500 rounded-lg">Create Column</button>
   </section>
+  <div class="grid grid-cols-4 gap-2 mx-1 ">
+    <div class="h-96 bg-gray-500 rounded-sm" v-for="column in board.columns">
+      <h2 class='text-center'>{{ column.name }}</h2>
+      <input type="text" class="border border-2  p-2 mx-2" v-model="newCardName" />
+      <button @click="createCard(column)" class="bg-red-500 rounded-lg">Create Card</button>
+
+      <ul>
+        <li v-for="card in column.cards">
+          {{ card.name }}</li>
+      </ul>
+    </div>
+
+  </div>
 </template>
