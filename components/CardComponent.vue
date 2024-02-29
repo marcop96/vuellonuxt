e14ea7aaa32d48bcb096e6e75e1805b1861fbf17
“Marco <marco.pianaroli96@gmail.com>
2024-02-29 17:25:07 +0100

    creates Card component

---
+++ components/CardComponent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = defineProps({
+  id: Number,
+  name: String,
+  column: String,
+});
+
+console.log(props);
+</script>
+
+<template>
+  <main class="card-background">
+    <p>{{ props.name }}</p>
+
+    <pre>{{ props }}</pre>
+  </main>
+</template>
+
+<style scoped>
+.card-background {
+  background-color: #f5f5f5;
+  margin-bottom: 4px;
+}
+</style>
