4b40cf568d29878e441ca9866eb71f1f9d66c8da
“Marco <marco.pianaroli96@gmail.com>
2024-03-03 13:20:41 +0100

    style: modifies index elements

--- components/actionButton.vue
+++ components/actionButton.vue
@@ -1,12 +1,13 @@
-<script setup lang='ts'>
-import { ref } from 'vue';
+<script setup lang="ts">
+import { ref } from "vue";
 const props = defineProps({
   text: String,
-  color: String
-})
-
+  color: String,
+});
 </script>
 
 <template>
-  <button class="bg-red-500 rounded-sm p-2 m-2">{{ props.text }}</button>
+  <button class="bg-red-500 rounded-sm p-2 m-2">
+    {{ props.text }}
+  </button>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
-<script setup lang='ts'>
-import { ref } from 'vue';
-import { workspaceList } from '../store/useStore';
-import ActionButton from '../components/actionButton.vue';
+<script setup lang="ts">
+import { ref } from "vue";
+import { workspaceList } from "../store/useStore";
+import ActionButton from "../components/actionButton.vue";
 
-
-
-const newWorkspaceName = ref('');
+const newWorkspaceName = ref("");
 
 function createWorkspace() {
   const randomId = Math.floor(Math.random() * 100);
@@ -14,39 +12,57 @@ function createWorkspace() {
     id: randomId,
     name: newWorkspaceName.value,
   });
-  newWorkspaceName.value = '';
+  newWorkspaceName.value = "";
 }
-
 </script>
 
 <template>
-  <h1>VUELLO</h1>
-  <h2>recently viewed
-  </h2>
-  <h2>workspaces</h2>
-  <input type="text" class="custom-input" placeholder="workspace name..." v-model="newWorkspaceName"
-    @keyup.enter="createWorkspace()" />
-  <ActionButton text="create workspace" @click="createWorkspace" />
+  <div class="flex flex-col justify-center items-center">
+    <input
+      type="text"
+      class="custom-input"
+      placeholder="workspace name..."
+      v-model="newWorkspaceName"
+      @keyup.enter="createWorkspace()"
+    />
+    <ActionButton
+      text="create workspace"
+      class="w-fit"
+      @click="createWorkspace"
+    />
+  </div>
   <ul>
-    <li v-for="workspace in workspaceList" :key='workspace.id' class="m-2 p-2 text-white border-2 block text-center">
-      <nuxt-link :to="'/workspace/' + workspace.id" :workspace="workspace" class="p-2 my-4 block w-full">
-        {{ workspace.name }} - {{ workspace.id }}</nuxt-link>
-
+    <li
+      v-for="workspace in workspaceList"
+      :key="workspace.id"
+      class="m-2 p-2 text-white border-2 w-72 text-center"
+    >
+      <nuxt-link
+        :to="'/workspace/' + workspace.id"
+        :workspace="workspace"
+        class="p-2 my-4 block w-full"
+      >
+        {{ workspace.name }} - {{ workspace.id }}</nuxt-link
+      >
     </li>
   </ul>
 </template>
 
-<style scoped> h1 {
-   color: white;
-   font-size: 36px;
- }
+<style scoped>
+h1 {
+  color: white;
+  font-size: 36px;
+}
 
- input {
-   color: #333;
-   background-color: #f4f4f4;
-   border: 1px solid #333;
-   margin: 5px;
-   padding: 5px;
-   font-size: 0.9rem;
- }
+input {
+  color: #333;
+  text-align: center;
+  height: 40px;
+  width: 360px;
+  background-color: #f4f4f4;
+  border: 1px solid #333;
+  margin: 5px;
+  padding: 5px;
+  font-size: 0.9rem;
+}
 </style>
